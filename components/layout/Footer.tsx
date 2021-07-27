b0bda9fd71df3605087b23f9cdaa5500a8c78c79
Chris Ragland <Chrisragland97@gmail.com>
2021-07-27 13:15:03 -0400

    work on footer, work on styles for banner

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,11 +4,6 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
       <p className={styles.copy}>&copy; 2021 Vidal Software</p>
     </footer>
   );
