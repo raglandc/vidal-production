35dad26c7fbc5f41edf034f3c1b7c714fb5998fe
Chris Ragland <Chrisragland97@gmail.com>
2021-07-27 09:29:03 -0400

    create card element, create css for card

---
+++ components/UI/Card.tsx
@@ -0,0 +1,12 @@
+//styles
+import styles from "./Card.module.css";
+
+type CardProps = {
+  children?: React.ReactNode;
+};
+
+const Card = ({ children }: CardProps) => {
+  return <div className={styles.card}>{children}</div>;
+};
+
+export default Card;
