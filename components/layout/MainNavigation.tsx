c9ee112394cb6ec349e31e3dff553c26f09bcaf7
Chris Ragland <Chrisragland97@gmail.com>
2021-08-03 08:11:06 -0400

    responsive nav bar completed with css

--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -1,6 +1,7 @@
-//components
+//library
 
-import { useState, useEffect } from "react";
+//components
+import { useState } from "react";
 import Logo from "../sections/sectionComponents/Logo";
 import MobileNavIcon from "../sections/sectionComponents/MobileNavIcon";
 import ListNavigation from "../sections/sectionComponents/ListNavigation";
@@ -8,31 +9,13 @@ import ListNavigation from "../sections/sectionComponents/ListNavigation";
 //styles
 import styles from "./MainNavigation.module.css";
 
-const mediaQuery = "screen and (min-width: 1000px)";
-
 ////// component /////////
 const MainNavigation = () => {
-  //responsive navbar
-  const mql = window.matchMedia(mediaQuery);
-  const [isDesktop, setIsDesktop] = useState(mql.matches);
-
-  useEffect(() => {
-    const mediaChangeHandler = function (this: MediaQueryList) {
-      setIsDesktop(this.matches);
-    };
-
-    mql.addEventListener("change", mediaChangeHandler);
-    setIsDesktop(mql.matches);
-
-    return () => {
-      mql.removeEventListener("change", mediaChangeHandler);
-    };
-  }, [mql]);
-
   return (
     <nav className={styles.navigation}>
       <Logo />
-      {isDesktop ? <ListNavigation /> : <MobileNavIcon />}
+      <ListNavigation />
+      <MobileNavIcon />
     </nav>
   );
 };
