e6c81e1a9aaa075913c3f69d29650b5022f1cd95
Chris Ragland <Chrisragland97@gmail.com>
2021-08-03 05:24:52 -0400

    responsive nav bar now works for mobile and desktop menus

--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -1,6 +1,6 @@
 //components
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "../sections/sectionComponents/Logo";
 import MobileNavIcon from "../sections/sectionComponents/MobileNavIcon";
 import ListNavigation from "../sections/sectionComponents/ListNavigation";
@@ -8,14 +8,31 @@ import ListNavigation from "../sections/sectionComponents/ListNavigation";
 //styles
 import styles from "./MainNavigation.module.css";
 
+const mediaQuery = "screen and (min-width: 1000px)";
+
 ////// component /////////
 const MainNavigation = () => {
-  const [isMobile, setIsMobile] = useState(true);
+  //responsive navbar
+  const mql = window.matchMedia(mediaQuery);
+  const [isDesktop, setIsDesktop] = useState(mql.matches);
+
+  useEffect(() => {
+    const mediaChangeHandler = function (this: MediaQueryList) {
+      setIsDesktop(this.matches);
+    };
+
+    mql.addEventListener("change", mediaChangeHandler);
+    setIsDesktop(mql.matches);
+
+    return () => {
+      mql.removeEventListener("change", mediaChangeHandler);
+    };
+  }, [mql]);
 
   return (
     <nav className={styles.navigation}>
       <Logo />
-      {!isMobile ? <MobileNavIcon /> : <ListNavigation />}
+      {isDesktop ? <ListNavigation /> : <MobileNavIcon />}
     </nav>
   );
 };
