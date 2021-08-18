57d0f7b6671e88648a430b76661d40c64990556e
Chris Ragland <Chrisragland97@gmail.com>
2021-08-18 07:50:22 -0400

    adding files

---
+++ components/UI/Logo.tsx
@@ -0,0 +1,34 @@
+//library imports
+import Image from "next/image";
+
+//state management
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  selectStatus,
+  setMenuStatus,
+} from "../../app/features/menuStatusSlice";
+
+//local imports
+import LogoSvg from "../../public/vidal.svg";
+
+//styles
+import styles from "./Logo.module.css";
+
+const Logo = () => {
+  const status = useAppSelector(selectStatus);
+  const dispatch = useAppDispatch();
+
+  const closeMenuHandler = () => {
+    if (status) {
+      dispatch(setMenuStatus());
+    }
+  };
+
+  return (
+    <div onClick={closeMenuHandler} className={styles.logo_container}>
+      <Image src={LogoSvg} alt="Vidal Logo" />
+    </div>
+  );
+};
+
+export default Logo;
