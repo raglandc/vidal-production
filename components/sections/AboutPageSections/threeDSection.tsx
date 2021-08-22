31ac65d0988def067361a35b208418c807b01c3c
Chris Ragland <Chrisragland97@gmail.com>
2021-08-21 21:36:14 -0400

    work on about page, need to add animation to vidal sign

--- components/sections/AboutPageSections/threeDSection.tsx
+++ components/sections/AboutPageSections/threeDSection.tsx
@@ -1,12 +1,22 @@
+//library
+import Image from "next/image";
+//local
+import LogoSvg from "../../../public/vidalWhite.svg";
+
 //styles
 import styles from "./threeDSection.module.css";
 const ThreeDSection = () => {
   return (
     <div className={styles.container}>
-      <h3 className={styles.header}>3D</h3>
-      <h3 className={styles.header}>Dream</h3>
-      <h3 className={styles.header}>Design</h3>
-      <h3 className={styles.header}>Develope</h3>
+      <div className={styles.headerContainer}>
+        <h3 className={styles.headers}>3D</h3>
+        <h3 className={styles.headers}>Dream</h3>
+        <h3 className={styles.headers}>Design</h3>
+        <h3 className={styles.headers}>Develope</h3>
+      </div>
+      <div className={styles.logo}>
+        <Image src={LogoSvg} alt="logo" width="200.85px" height="325px" />
+      </div>
     </div>
   );
 };
