c99f5435c640091d8dce2876fd220202687d0d1c
Chris Ragland <Chrisragland97@gmail.com>
2021-08-28 21:59:56 -0400

    footer icons sizing fix

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,9 +1,25 @@
+//library
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 //styles
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div>
+        <Link href={"https://www.linkedin.com/in/chris-ragland-42461a1b4/"}>
+          <a target="_blank">
+            <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+          </a>
+        </Link>
+        <Link href={"https://github.com/raglandc"}>
+          <a target="_blank">
+            <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+          </a>
+        </Link>
+      </div>
       <p className={styles.copy}>&copy; 2021 Vidal </p>
     </footer>
   );
