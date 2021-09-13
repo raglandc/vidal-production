d5f2d7fb5c5040c1a5a8cf083c072bc3f76db312
Chris Ragland <Chrisragland97@gmail.com>
2021-09-13 11:36:18 -0400

    work on banner image looks amazing bro

--- components/sections/HomePageSections/BannerSection.tsx
+++ components/sections/HomePageSections/BannerSection.tsx
@@ -1,8 +1,10 @@
 //library imports
 import { useRef } from "react";
 import useIntersectionObserver from "../../../hooks/useIntersectionObserver";
+import Image from "next/image";
 
-//components
+//local
+import BannerSvg from "../../../public/images/home/bannerSVG.svg";
 import Button from "../../UI/Button";
 //styles
 
@@ -15,25 +17,36 @@ const BannerSection = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.titleWrapper}>
-        <h1 ref={ref} className={isVisible ? styles.title : styles.titleVidal}>
-          Vidal
-        </h1>
+      <div className={styles.divider}>
+        <div className={styles.titleWrapper}>
+          <h1
+            ref={ref}
+            className={isVisible ? styles.title : styles.titleVidal}
+          >
+            Vidal
+          </h1>
+        </div>
+        <div
+          ref={ref}
+          className={isVisible ? styles.subHeading : styles.subHeadingStart}
+        >
+          <p className={styles.subHeading__text}>
+            A powerful online presence is important.
+          </p>
+          <p className={styles.subHeading__text}>These days, its vital.</p>
+          <p className={styles.subHeading__text}>Meet Vidal.</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button link="/about" title="Learn More" style="hollow" />
+          <Button link="/contact" title="Contact" style="solid" />
+        </div>
       </div>
-      <div
-        ref={ref}
-        className={isVisible ? styles.subHeading : styles.subHeadingStart}
-      >
-        <p className={styles.subHeading__text}>
-          A powerful online presence is important.
-        </p>
-        <p className={styles.subHeading__text}>These days, its vital.</p>
-        <p className={styles.subHeading__text}>Meet Vidal.</p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <Button link="/about" title="Learn More" style="hollow" />
-        <Button link="/contact" title="Contact" style="solid" />
-      </div>
+      <Image
+        src={BannerSvg}
+        alt="personal website cartoon"
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
