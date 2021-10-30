8f6062c9f6ffc5a2ae50f301631d594f11cf3b8d
Chris Ragland <Chrisragland97@gmail.com>
2021-10-30 16:04:30 -0400

    v.1.1

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,7 +5,7 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 ////////////////////////////////////////////////////////////////
-//EVERYTHING WORKS, JUST TURN ON LESS SECURE APPS TO USE GMAIL//
+//EVERYTHING WORKS, USING SEND GRID IN PRODUCTION, CHECK SPAM //
 ////////////////////////////////////////////////////////////////
 
 export default function contactHandler(
@@ -17,16 +17,18 @@ export default function contactHandler(
   const mssg = {
     to: `${email}`,
     from: `vidalteamcontact@gmail.com`,
-    subject: `Thank you for reaching out`,
-    text: `We got your message! We will respond shortly to the email you gave us.`,
+    subject: `Thank you for reaching out ${name}`,
+    text: `We got your message!\n We will respond shortly to the email you gave us.`,
   };
 
   const mssgToVidal = {
     to: `vidalteamcontact@gmail.com`,
     from: `vidalteamcontact@gmail.com`,
     subject: `Email from ${name}`,
-    text: `${message}, from: ${email}`,
+    text: `${message}\n from: ${email}`,
   };
+
+  //email to user
 
   sgMail
     .send(mssg)
@@ -39,6 +41,8 @@ export default function contactHandler(
       res.status(500).json({ message: "Failed to send email to user" });
       console.error(error);
     });
+
+  //email to self, check spam folder.
 
   sgMail
     .send(mssgToVidal)
